c6981b88e28628488a238243183930a184d5b776
Armando Andini <armando.andini@hotmail.com>
2018-03-29 16:01:28 -0300

    Minesweeper react app

---
+++ src/App.js
@@ -0,0 +1,95 @@
+import React, { Component } from 'react';
+import {Minefield, MineTile} from './Minefield.js';
+import logo from './logo.svg';
+import './App.css';
+
+
+class RevealedTile extends Component {
+  renderSymbol = () => {
+    var tile = this.props.tile
+    if (tile.mined)
+      return 'x'
+    else if (tile.isClear())
+      return ''
+    else
+      return tile.adjacentMines
+  }
+
+  render() {
+    var tile = this.props.tile
+    var classes = 'tile revealed'
+    classes += ' count' + tile.adjacentMines
+    if (tile.mined) classes += ' mined'
+
+    return <div className={classes}>
+      { this.renderSymbol() }
+    </div>
+  }
+}
+
+class UnrevealedTile extends Component {
+  revealTile =() =>{
+    this.props.tile.chainReveal()
+    this.props.onClicked()
+  }
+
+  render() {
+    return <div className='tile' onClick={this.revealTile}>
+    </div>
+  }
+}
+
+class Tile extends Component {
+  render() {
+    var tile = this.props.tile
+    return tile.revealed ? <RevealedTile tile={tile}/> : <UnrevealedTile tile={tile} onClicked={this.props.reRender}/>
+  }
+}
+
+window.minefield = new Minefield()
+
+class App extends Component {
+  componentWillMount(){
+    this.setState({ tiles: window.minefield.tiles })
+  }
+
+  reRender = ()=>{
+    this.forceUpdate()
+  }
+
+  renderTiles(){
+    const tiles = []
+
+    for(let row of this.state.tiles){
+      for(let tile of row){
+        tiles.push(
+          <Tile key={tiles.length} tile={tile} reRender={this.reRender} />
+        )
+      }
+    }
+    return tiles
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <h1 className="App-title">Welcome to Minesweeper</h1>
+        </header>
+      <div className='tile-container'>
+        { /* this.renderTiles() */ }
+        { this.state.tiles.map( row => (
+          <div style={{display: 'table-row'}}>
+            { row.map( tile => (
+              <Tile tile={tile} reRender={this.reRender} />
+            ))}
+          </div>
+        ))}
+      </div>
+      </div>
+    );
+  }
+}
+
+export default App;
---
+++ src/Minefield.js
@@ -0,0 +1,91 @@
+const columns = 10
+const rows = 10
+
+class Minefield {
+  constructor(){
+    this.tiles = this.generateMineField()
+    this.calculateAdjacentMines()
+  }
+
+  getTile(x, y){
+    if(x >= 0 && x < columns && y >= 0 && y < rows){
+      return this.tiles[x][y]
+    }
+  }
+
+  generateMineField(){
+    var tiles = []
+    for (var i = 0; i < rows; i++) {
+      tiles[i] = []
+      for( var j = 0; j < columns; j++) {
+        var mined = Math.random() > 0.8
+        tiles[i].push(new MineTile(this,i,j))
+      }
+    }
+    return tiles
+  }
+
+  calculateAdjacentMines(){
+    for (var i = 0; i < rows; i++) {
+      for( var j = 0; j < columns; j++) {
+        var tile = this.getTile(j,i)
+        if(tile.mined){
+          tile.getAllNeighbors().forEach(tile => tile.increaseAdjacentMines())
+        }
+      }
+    }
+  }
+}
+
+class MineTile {
+  constructor(minefield, row, column){
+    this.minefield = minefield
+    this.row = row
+    this.column = column
+    this.mined = Math.random() > 0.9
+    this.revealed = false
+    this.adjacentMines = 0
+  }
+
+  reveal(){
+    this.revealed = true
+  }
+
+  chainReveal(){
+    if (this.revealed) return
+    this.reveal()
+    if(this.isClear())
+      this.getCrossNeighbors().forEach(tile => tile.chainReveal())
+  }
+
+
+  getNeighbors(offsets){
+    var neighbors = []
+    offsets.forEach(offset => {
+      var neighbor = this.minefield.getTile(this.row + offset[0], this.column + offset[1])
+      neighbor && neighbors.push(neighbor)
+    })
+    return neighbors
+  }
+
+  getAllNeighbors(){
+    const offsets = [[-1,-1],[0,-1],[1,-1],[-1,0],[1,0],[-1,1],[0,1],[1,1]]
+    return this.getNeighbors(offsets)
+  }
+
+  getCrossNeighbors(){
+    const offsets = [[0,-1],[-1,0],[1,0],[0,1]]
+    return this.getNeighbors(offsets)
+  }
+
+  increaseAdjacentMines(){
+    this.adjacentMines += 1
+  }
+
+  isClear(){
+    return this.adjacentMines == 0
+  }
+
+}
+
+export { Minefield, MineTile }
